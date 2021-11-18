f0149c4b7a95214ce0e4dc4036782187164f37fc
Juan Pablo De la torre Valdez <ptorre@creativaweb.mx>
2021-11-18 17:13:39 -0600

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {""}
-            <span className="text-indigo-600">Veterinaria</span>
+            Seguimiento Pacientes {""} Con Github {''}
+            <span className="text-indigo-700">Veterinaria</span>
         </h1>
     )
 }
